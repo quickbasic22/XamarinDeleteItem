68862800ccbecc2be01ab710fffe12f3efdb4eba
David Morrow <quickbasic44@outlook.com>
2022-09-18 16:21:46 -0400

    Add project files.

---
+++ XamarinDeleteItem/XamarinDeleteItem/Views/ItemsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+using XamarinDeleteItem.Models;
+using XamarinDeleteItem.ViewModels;
+using XamarinDeleteItem.Views;
+
+namespace XamarinDeleteItem.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+
+        private void DeleteClick_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XamarinDeleteItem/XamarinDeleteItem/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinDeleteItem.Models;
+using XamarinDeleteItem.ViewModels;
+
+namespace XamarinDeleteItem.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
