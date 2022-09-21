127ced10235379f857177130bed4f1f3ce704b5d
David Morrow <quickbasic44@outlook.com>
2022-09-21 17:35:07 -0400

    SwipeView invoked in CodeBehind Delete Item works

--- XamarinDeleteItem/XamarinDeleteItem/Views/ItemsPage.xaml.cs
+++ XamarinDeleteItem/XamarinDeleteItem/Views/ItemsPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
@@ -23,12 +25,22 @@ namespace XamarinDeleteItem.Views
             BindingContext = _viewModel = new ItemsViewModel();
         }
 
+      
+
         protected override void OnAppearing()
         {
             base.OnAppearing();
             _viewModel.OnAppearing();
         }
 
-        
+        private void SwipeItem_Invoked(object sender, EventArgs e)
+        {
+            var item = sender as SwipeItem;
+
+            var i = item.BindingContext as Item;
+
+            _viewModel.Items.Remove(i);
+            _viewModel.DataStore.DeleteItemAsync(i.Id);
+        }
     }
 }
